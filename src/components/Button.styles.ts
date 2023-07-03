43a1a51e5bab958971ffe6760c4f6c0b563911cf
Giovana de Miranda <giovana.miranda@bitize.com.br>
2023-07-03 12:27:20 -0300

    finalização primeiro modulo

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export type ButtonVariant =  'primary' |  'secondary' | 'danger' | 'success';
 
@@ -17,8 +17,10 @@ const buttonVariants  = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-  ${props => {
-    return css`background-color: ${buttonVariants[props.variant]}`
-  }}
-`
+  background-color: ${props => props.theme['green-500']};
+  color: ${props =>props.theme.white}
+`;
